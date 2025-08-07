fa74843e532c9c0d4e74ffcf9952bd55fc822347
AmanthaSanuja <amanthasanuja9@gmail.com>
2025-08-07 23:15:40 +0530

    motion commented

--- To-Do-List/src/components/ToDoList.jsx
+++ To-Do-List/src/components/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 const API_URL = 'http://localhost:8080/api/todos';
 
